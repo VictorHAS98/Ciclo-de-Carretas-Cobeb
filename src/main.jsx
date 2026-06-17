434adf9c3399151f16a54f7f43eb172970fd8730
VictorHugo <fantasyvhcf@gmail.com>
2026-06-17 04:43:09 -0300

    chore: configure GitHub Pages deploy with SPA routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Ciclo-de-Carretas-Cobeb">
       <App />
     </BrowserRouter>
   </React.StrictMode>
